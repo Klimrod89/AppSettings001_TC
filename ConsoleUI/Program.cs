df61bf4c7cd33171372256506ba10125b24c2673
Yassir C <51804657+Klimrod89@users.noreply.github.com>
2024-05-18 19:49:18 +0100

    First commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", false, true)
+                                .AddJsonFile("appSettings.Development.json", true, true)
+                                .AddUserSecrets<Program>();
+
+IConfigurationRoot config = builder.Build();
+
+Console.WriteLine(config["MySetting"] ?? "There is no settings");
+Console.WriteLine(config.GetValue<string>("MainSetting:SubSetting"));
+Console.WriteLine(config.GetValue<string>("MainSetting:SubSection:SubSubSetting"));
+Console.WriteLine(config.GetConnectionString("Default"));
+Console.ReadLine();
